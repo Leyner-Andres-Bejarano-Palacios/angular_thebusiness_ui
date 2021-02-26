ed27f155c05b9dd7282985e6ba284f6f5b9eaea9
Leyner Andres Bejarano Palacios <personaltraingcp@gmail.com>
2021-02-26 10:45:29 -0500

    :construction: doing the tour of heroes, changing tittle name

--- BussinessUI/src/app/app.component.ts
+++ BussinessUI/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BussinessUI';
+  title = 'this is a the tittle BussinessUI been changed'; 
 }
